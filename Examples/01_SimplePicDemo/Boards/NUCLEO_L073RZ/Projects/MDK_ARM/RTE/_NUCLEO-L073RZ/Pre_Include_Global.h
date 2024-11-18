ec15175a6aa11c553ebc659954d2cb48c1ac0837
Maxim Hodnenko <testingon2022@gmail.com>
2024-11-18 15:29:30 +0300

    Initial default project for 01 Simple demo and Nucleo L073RZ board Signed-off-by: Maxim Hodnenko <testingon2022@gmail.com>

---
+++ Examples/01_SimplePicDemo/Boards/NUCLEO_L073RZ/Projects/MDK_ARM/RTE/_NUCLEO-L073RZ/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SimplePic' 
+ * Target:  'NUCLEO-L073RZ' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil::Device&STM32CubeMX:STM32Cube HAL@1.12.2 */
+#define USE_HAL_DRIVER
+          #define USE_FULL_LL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ Examples/01_SimplePicDemo/Boards/NUCLEO_L073RZ/Projects/MDK_ARM/RTE/_NUCLEO-L073RZ/RTE_Components.h
@@ -0,0 +1,29 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SimplePic' 
+ * Target:  'NUCLEO-L073RZ' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.9.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil::Device&STM32CubeMX:STM32Cube Framework:STM32CubeMX@1.12.2 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device&STM32CubeMX:STM32Cube HAL@1.12.2 */
+#define RTE_DEVICE_CUBE_MX_HAL
+
+
+#endif /* RTE_COMPONENTS_H */
